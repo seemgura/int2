47e347b6c54810b0ebb9f7956ba4ad1c71e61d2b
seemgura <seemgura@gmail.com>
2025-07-14 18:01:11 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -14,4 +15,5 @@ def index():
     return render_template("index.html", estilos=estilos)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
